28db3e0ee3636c0794ab70db5b70e41f39301b16
Ruben Moreno <rmxtronic@gmail.com>
2025-10-29 05:29:49 -0500

    creacion de packetes, controller y repos

---
+++ src/main/java/MyBudgetApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBudgetApplication.class, args);
+	}
+
+}
